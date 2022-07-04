40faad5db89f1f41ee3dd167aa8be6a520f640f7
Emeka02 <eluemchukwuemeka@gmail.com>
2022-07-03 17:03:08 -0700

    done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+* main - Prints a random number and states whether*
+* it is positive, negative, or zero.
+* Return: Always 0
+*/
 
 int main(void)
 {
