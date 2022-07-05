cf25e1e58f06488cc2b9d5bed4c8cc39069482b2
Emeka02 <eluemchukwuemeka@gmail.com>
2022-07-04 18:16:08 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
+/**
+* main - entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-
-/** 
- *
- *main - Entry Point 
- *Return: 1 (Failure)
- */
- int main(void)
-
- {
-write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
- }
+	write(2, str1, 59);
+	return (1);
+}
