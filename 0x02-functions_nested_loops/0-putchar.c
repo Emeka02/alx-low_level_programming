329794126395d3cc2c76cedd76b3e5bde8b7eb8c
Emeka02 <eluemchukwuemeka@gmail.com>
2022-07-06 16:02:49 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*main - prints "_putchar"
+*
+* Return: On success 1.
+*/
+int main(void)
+{
+	char s[] = "_putchar";
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
+
+	return (0);
+}
