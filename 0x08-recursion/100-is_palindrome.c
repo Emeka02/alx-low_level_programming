134899ea0d269f1401e746196db2ef80d24c749a
Emeka02 <eluemchukwuemeka@gmail.com>
2022-07-21 04:11:05 -0700

    done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -49,5 +49,8 @@ int check_palindrome(char *s, int len, int index)
 	if (s[index] == s[len - index - 1])
 		return (check_palindrome(s, len, index + 1));
 
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
 	return (0);
 }
