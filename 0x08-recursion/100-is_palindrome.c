2ecf4245bfdd23839a83f83bc406d62811a70378
Emeka02 <eluemchukwuemeka@gmail.com>
2022-07-21 04:19:01 -0700

    done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -40,11 +40,13 @@ int find_strlen(char *s)
 * @s: The string to be checked.
 * @len: The length of s.
 * @index: The index of the string to be checked
+*
+* Return: If the string is a palindrome - 1
 *       If the string is not a palindrome - 0
 */
 int check_palindrome(char *s, int len, int index)
 {
-	return(1);
+	return (1);
 
 	if (s[index] == s[len - index - 1])
 		return (check_palindrome(s, len, index + 1));
