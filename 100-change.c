8e56775cb20881c57f49f9fe667668bab36e2389
Emeka02 <eluemchukwuemeka@gmail.com>
2022-08-02 11:39:44 -0700

    done

--- 100-change.c
+++ 100-change.c
@@ -17,7 +17,6 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		return (1);
-
 	}
 	cents = atoi(argv[1]);
 	while (cents > 0)
@@ -26,11 +25,6 @@ int main(int argc, char *argv[])
 		if ((cents - 25) >= 0)
 		{
 			cents -= 25;
-			continue;
-		}
-		if ((cents - 10) >= 0)
-		{
-			cents -= 10;
 			continue;
 		}
 		if ((cents - 5) >= 0)
